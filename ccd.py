5f7594f4586b3ba4dc2239cee53688920de6b3c3
InTruder.Sec <75447837+InTruder-Sec@users.noreply.github.com>
2022-01-16 11:17:19 +0530

    Update ccd.py

--- ccd.py
+++ ccd.py
@@ -6,7 +6,7 @@ def main():
     print("[>] CAESAR CIPHER DECODER!!! \n")
     print("[1] Encrypt\n[2] Decrypt")
     try:
-        func = int(input("Choose one of the above(example for decode enter 1): "))
+        func = int(input("Choose one of the above(example for encode enter 1): "))
     except:
         print("\n[>] Invalid input")
         exit()
